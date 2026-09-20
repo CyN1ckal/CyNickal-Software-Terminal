4572876427e13ef1677e213e4765c00891a9cbfe
CyNickal <cyn1ckal@users.noreply.github.com>
2026-09-20 16:19:09 -0700

    Added CBarSeries abstraction for market-data

---
+++ libs/market-data/src/CBarSeries.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cynickal on 9/19/26.
+//
+
+#include "CBarSeries.h"
--- libs/market-data/src/market-data.h
+++ libs/market-data/src/market-data.h
@@ -5,14 +5,13 @@
 #ifndef MYAPP_MARKET_DATA_H
 #define MYAPP_MARKET_DATA_H
 
-#include "CBarData.h"
+#include "CBarSeries.h"
 
 #include <cstddef>
 #include <string>
-#include <vector>
 
 namespace market_data{
-    const std::vector<CBarData> GetBarData(const std::string& Symbol, std::size_t NumBars);
+    CBarSeries GetBarData(const std::string& Symbol, std::size_t NumBars);
 }
 
 #endif //MYAPP_MARKET_DATA_H
